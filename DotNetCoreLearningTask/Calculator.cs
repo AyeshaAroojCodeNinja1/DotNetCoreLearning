e6dc29a7be2abd195e10ffc8c308216dfaff28cb
Ayesha Arooj <ayesha.arooj@codeninjaconsulting.com>
2023-10-31 10:13:19 +0500

    Task 1

---
+++ DotNetCoreLearningTask/Calculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreLearningTask
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            int result = 0;
+            for (int i = 0; i < b; i++)
+            {
+                result += a;
+            }
+            return b < 0 ? -result : result;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (a == 0)
+                return 0;
+            if (b == 0)
+                throw new DivideByZeroException("Division by zero is not allowed.");
+
+            bool negResult = false;
+
+            // Handling negative numbers 
+            if (a < 0)
+            {
+                a = -a;
+                if (b < 0)
+                    b = -b;
+                else
+                    negResult = true;
+            }
+            else if (b < 0)
+            {
+                b = -b;
+                negResult = true;
+            }
+
+            // if a is greater than equal to b , subtract b from a and increase quotient by one. 
+            int quotient = 0;
+            while (a >= b)
+            {
+                a = a - b;
+                quotient++;
+            }
+
+            // checking if neg equals to 1 then making  quotient negative  
+            if (negResult)
+                quotient = -quotient;
+            return quotient;
+        }
+    }
+}
--- DotNetCoreLearningTask/Program.cs
+++ DotNetCoreLearningTask/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DotNetCoreLearningTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calculator = new Calculator();
+        while (true)
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+            Console.WriteLine("5. Exit");
+            Console.Write("Please choose an option (1-5): ");
+
+            if (int.TryParse(Console.ReadLine(), out int operation))
+            {
+                if (operation >= 1 && operation <= 4)
+                {
+                    Console.Write("Enter the first number: ");
+                    if (int.TryParse(Console.ReadLine(), out int num1))
+                    {
+                        Console.Write("Enter the second number: ");
+                        if (int.TryParse(Console.ReadLine(), out int num2))
+                        {
+                            try
+                            {
+                                switch (operation)
+                                {
+                                    case 1:
+                                        Console.WriteLine($"Result: {calculator.Add(num1, num2)}");
+                                        break;
+                                    case 2:
+                                        Console.WriteLine($"Result: {calculator.Subtract(num1, num2)}");
+                                        break;
+                                    case 3:
+                                        Console.WriteLine($"Result: {calculator.Multiply(num1, num2)}");
+                                        break;
+                                    case 4:
+                                        Console.WriteLine($"Result: {calculator.Divide(num1, num2)}");
+                                        break;
+                                }
+                            }
+                            catch (DivideByZeroException ex)
+                            {
+                                Console.WriteLine($"Error: {ex.Message}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a valid number.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number.");
+                    }
+                }
+                else if (operation == 5)
+                {
+                    Console.WriteLine("--- Program Terminated ---");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please select a valid option (1-5).");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid option (1-5).");
+            }
+        }
+    }
+}
