35f866d6184ebf733222a06b685a01b174d430d9
Ayesha Arooj <ayesha.arooj@codeninjaconsulting.com>
2023-10-31 16:00:34 +0500

    Added DisplayMenu( ) function, break out conditions . added helper class

---
+++ DotNetCoreLearningTask/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreLearningTask
+{
+    public static class Helper
+    {
+        public static bool ValidateValidInput(out int result, string prompt)
+        {
+            Console.Write(prompt);
+            if (int.TryParse(Console.ReadLine(), out result))
+            {
+                return true;
+            }
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+            return false;
+        }
+    }
+}
--- DotNetCoreLearningTask/Program.cs
+++ DotNetCoreLearningTask/Program.cs
@@ -8,75 +8,57 @@ class Program
         ICalculate calculator = null;      
         while (true)
         {
-            Console.WriteLine("Menu:");
-            Console.WriteLine("1. Add");
-            Console.WriteLine("2. Subtract");
-            Console.WriteLine("3. Multiply");
-            Console.WriteLine("4. Divide");
-            Console.WriteLine("5. Exit");
-            Console.Write("Please choose an option (1-5): ");
+            DisplayMenu();
 
-            if (int.TryParse(Console.ReadLine(), out int operation))
-            {
-                if (operation >= 1 && operation <= 4)
-                {
-                    Console.Write("Enter the first number: ");
-                    if (int.TryParse(Console.ReadLine(), out int num1))
-                    {
-                        Console.Write("Enter the second number: ");
-                        if (int.TryParse(Console.ReadLine(), out int num2))
-                        {
-                            try
-                            {
-                                switch (operation)
-                                {
-                                    case 1:
-                                        calculator = new Add();
-                                        Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
-                                        break;
-                                    case 2:
-                                        calculator = new Subtract();
-                                        Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
-                                        break;
-                                    case 3:
-                                        calculator = new Multiply();
-                                        Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
-                                        break;
-                                    case 4:
-                                        calculator = new Divide();
-                                        Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
-                                        break;
-                                }
-                            }
-                            catch (DivideByZeroException ex)
-                            {
-                                Console.WriteLine($"Error: {ex.Message}");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Invalid input. Please enter a valid number.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input. Please enter a valid number.");
-                    }
-                }
-                else if (operation == 5)
-                {
-                    Console.WriteLine("--- Program Terminated ---");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice. Please select a valid option (1-5).");
-                }
-            }
-            else
+            if (!int.TryParse(Console.ReadLine(), out int operation) || operation < 1 || operation > 5)
             {
                 Console.WriteLine("Invalid input. Please enter a valid option (1-5).");
             }
+            else if (operation == 5)
+            {
+                Console.WriteLine("--- Program Terminated ---");
+            }
+            else if (Helper.ValidateValidInput(out int num1, "Enter the first number: ") && Helper.ValidateValidInput(out int num2, "Enter the second number: "))
+            {
+                try
+                {
+                    switch (operation)
+                    {
+                        case 1:
+                            calculator = new Add();
+                            Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
+                            break;
+                        case 2:
+                            calculator = new Subtract();
+                            Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
+                            break;
+                        case 3:
+                            calculator = new Multiply();
+                            Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
+                            break;
+                        case 4:
+                            calculator = new Divide();
+                            Console.WriteLine($"Result: {calculator.Calculate(num1, num2)}");
+                            break;
+                            }
+                        }
+                        catch (DivideByZeroException ex)
+                        {
+                            Console.WriteLine($"Error: {ex.Message}");
+                        }               
+            }
+           
         }
+    }
+
+    public static void DisplayMenu()
+    {
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1. Add");
+        Console.WriteLine("2. Subtract");
+        Console.WriteLine("3. Multiply");
+        Console.WriteLine("4. Divide");
+        Console.WriteLine("5. Exit");
+        Console.Write("Please choose an option (1-5): ");
     }
 }
